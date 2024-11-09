a4eaf71ab5e19063ec171edc077297311558c7be
mateo200_2 <espanamateo599@gmail.com>
2024-11-09 18:35:02 -0500

    first commit

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "mateo");
-
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-} else {
-    echo "¡Conexión con éxito!";
-}
-?>
